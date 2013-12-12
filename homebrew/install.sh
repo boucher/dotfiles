03f85078968b51ad6a96d72f011f6014316baf5e
boucher <rboucher@gmail.com>
2013-12-11 18:19:19 -0800

    brew cask

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -16,4 +16,11 @@ fi
 # Install homebrew packages
 brew install grc coreutils spark
 
+# other shit
+brew install apple-gcc42 fltk go jpeg pcre swig autoconf freetype graphicsmagick libmpc pkg-config szip automake gd graphviz libpng qhull tbb brew-cask gfortran grc libtool qrupdate texinfo cloog git hadoop lua qt xz cmake glpk hdf5 maven readline coreutils gmp imagemagick mpfr sip dotwrp gnu-sed isl node spark fftw gnuplot jetty octave suite-sparse
+brew tap phinze/cask
+brew tap homebrew/science
+
+brew cask install air-video-server dropbox google-drive subethaedit colloquy google-chrome steam
+
 exit 0
