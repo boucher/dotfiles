446655ac9347527281dd142f665123be24480433
boucher <rboucher@gmail.com>
2013-12-11 18:06:28 -0800

    random changes.

---
+++ python/aliases.zsh
@@ -0,0 +1,1 @@
+alias ipython='nocorrect ipython'
---
+++ python/environment.zsh
@@ -0,0 +1,3 @@
+export PYLEARN2_DATA_PATH=~/Dropbox/Projects/data
+export PATH="$PATH:$PROJECTS/pylearn2/pylearn2/scripts"
+export PYLEARN2_VIEWER_COMMAND="open -nW"
