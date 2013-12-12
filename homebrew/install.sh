6facff26497ff844a8f7d0b4d96d2694e031a49e
boucher <rboucher@gmail.com>
2013-12-11 18:47:11 -0800

    remove dead links

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -17,10 +17,11 @@ fi
 brew install grc coreutils spark
 
 # other shit
-brew install apple-gcc42 fltk go jpeg pcre swig autoconf freetype graphicsmagick libmpc pkg-config szip automake gd graphviz libpng qhull tbb brew-cask gfortran grc libtool qrupdate texinfo cloog git hadoop lua qt xz cmake glpk hdf5 maven readline coreutils gmp imagemagick mpfr sip dotwrp gnu-sed isl node spark fftw gnuplot jetty octave suite-sparse
 brew tap phinze/cask
 brew tap homebrew/science
+brew install brew-cask
 
-brew cask install adium air-video-server dropbox google-drive subethaedit colloquy google-chrome steam sublime-text transmit transmission mumble firefox
+brew install apple-gcc42 fltk go jpeg pcre swig autoconf freetype graphicsmagick libmpc pkg-config szip automake gd graphviz libpng qhull tbb brew-cask gfortran grc libtool qrupdate texinfo cloog git hadoop lua qt xz cmake glpk hdf5 maven readline coreutils gmp imagemagick mpfr sip dotwrp gnu-sed isl node spark fftw gnuplot jetty octave suite-sparse
+brew cask install adium air-video-server dropbox colloquy google-chrome steam sublime-text transmit transmission mumble firefox
 
 exit 0
